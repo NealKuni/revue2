dac03a893f918b244cc88e376e0099fe29004871
nealichinohe <neal.kuni@yahoo.com>
2022-02-06 23:08:06 -0700

    first commit

---
+++ server/config/mongoose.config.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+// const uri = process.env.ATLAS_URI;
+
+module.exports = () => {
+    mongoose.connect( "mongodb://localhost/newrevue", { 
+        useNewUrlParser: true, 
+        useUnifiedTopology: true,
+        
+    })
+        .then(() => {
+            console.log("Established a connection to the database")
+            
+        })  
+        .catch(err => console.log("Something went wrong when connecting to the database", err));
+
+}
+
+const connection = mongoose.connection;
+connection.once("open", () => 
+console.log("MongoDB connnection established successfully")
+);
+
